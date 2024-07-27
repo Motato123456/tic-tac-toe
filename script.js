44a08e682a8e6154ebe238efb08372aaca5c40f2
Motato123456 <elliotyap00@gmail.com>
2024-07-27 11:12:25 +0800

    winning and tie checking is complete!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const gameBoard = (function (){
     let board = new Array(9).fill('');
     let displayBoard = function () {
@@ -38,12 +39,23 @@ const playgame = (function(){
     let gameOver;
     let currentPlayerIndex;
     const handleclick = (event) =>{
+        if (gameOver){
+            return
+        }
         let index = parseInt(event.target.className.split(' ')[1]);
         if (gameBoard.getGameboard()[index] !== '')
             return
         
         gameBoard.update(index, players[currentPlayerIndex].token)
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0 ;
+        if(checkForWin(gameBoard.getGameboard(),players[currentPlayerIndex].token)){
+            gameOver = true;
+            alert('playerwon')
+        }
+        else if(checkForTie(gameBoard.getGameboard())){
+            gameOver = true;
+            alert('its a tie!')
+        }
         
     }
     let gameStart = () => {
@@ -57,20 +69,52 @@ const playgame = (function(){
 
     }
     const restartGame = () => {
-        // Clear the game board array
+    
         gameBoard.getGameboard().fill('');
-        
-        // Clear the display of the game board
         let grid = document.querySelector('.container');
         grid.innerHTML = '';
-    
-        // Reinitialize the game
         gameStart();
     }
-    return {gameStart,handleclick,restartGame}
+    return {gameStart,handleclick,restartGame,}
 
 
 })();
+
+function checkForWin(board){
+    if (board[0] === board[1] && board[0] === board[2] && board[0] !== '') {
+        return true;
+    }
+    if (board[3] === board[4] && board[3] === board[5] && board[3] !== '') {
+        return true;
+    }
+    if (board[6] === board[7] && board[6] === board[8] && board[6] !== '') {
+        return true;
+    }
+
+    // Check columns
+    if (board[0] === board[3] && board[0] === board[6] && board[0] !== '') {
+        return true;
+    }
+    if (board[1] === board[4] && board[1] === board[7] && board[1] !== '') {
+        return true;
+    }
+    if (board[2] === board[5] && board[2] === board[8] && board[2] !== '') {
+        return true;
+    }
+
+    // Check diagonals
+    if (board[0] === board[4] && board[0] === board[8] && board[0] !== '') {
+        return true;
+    }
+    if (board[2] === board[4] && board[2] === board[6] && board[2] !== '') {
+        return true;
+    }
+
+    return false;
+}
+function checkForTie (board){
+    return board.every(cell => cell != '')
+}
 
 const startbutton = document.querySelector('.playgame-btn');
 startbutton.addEventListener('click', ()=>{
